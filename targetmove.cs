c1cae670032ac91df9391abb750a4a49668dd2ab
Suzy Adel Wahba <128175020+SuzyAdel@users.noreply.github.com>
2026-04-20 10:11:28 +0200

    Simple arrow movement

---
+++ targetmove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class targetmove : MonoBehaviour
+{
+    public float Speed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move target with arrow keys
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * Speed );
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * Speed );
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        }     
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * Speed);
+        }
+    }
+}
